5570de4169a66673f67c168f7fc3e2319620d1d6
Sharath <sharath.esr@gmail.com>
2024-10-24 17:32:36 +0530

    accord multiple-selection

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -1,26 +1,47 @@
 import { useState } from "react";
 import data from "./data";
 
-
 const Accordion = () => {
+  const [showIndex, setShowIndex] = useState(null);
+  const [multipleSelectionToggle, setMultipleSelectionToggle] = useState(false);
+  const [showIndices, setShowIndices] = useState([]);
 
-    const [showIndex, setShowIndex] = useState(null);
+  const handleToggle = (index) => {
+    if (multipleSelectionToggle) {
+      setShowIndices((prevState) =>
+        prevState.includes(index)
+          ? prevState.filter((i) => i !== index)
+          : [...prevState, index]
+      );
+    } else setShowIndex(index === showIndex ? null : index);
+  };
 
-    const handleToggle =(index) =>{
-        setShowIndex(index ===showIndex ? null : index)
-    }
+  const handleMultipleSelection = () => {
+    setMultipleSelectionToggle((prevState) => !prevState);
+    setShowIndex(null);
+    setShowIndices([]);
+  };
+
   return (
     <div>
       <h1 className="text-2xl my-7">Accordian component</h1>
+      <button
+        onClick={handleMultipleSelection}
+        className="p-4 bg-yellow-800 text-white rounded-lg"
+      >
+        {multipleSelectionToggle ? "Multiple Selection" : "Single Selection"}
+      </button>
       <ul>
-        {data.map((item,index) => (
+        {data.map((item, index) => (
           <li
             key={item.id}
-            className="my-5 rounded-lg cursor-pointer w-[50rem]"
-            onClick={()=>handleToggle(index)}
+            className="my-5 rounded-xl cursor-pointer w-[50rem]"
+            onClick={() => handleToggle(index)}
           >
-            <h1  className="p-4 bg-green-500  ">{item.question } </h1>
-            {showIndex===index && <p>{item.answer}</p>}
+            <h1 className="p-4 bg-green-500  ">{item.question} </h1>
+            {multipleSelectionToggle
+              ? showIndices.includes(index) && <p>{item.answer}</p>
+              : showIndex === index && <p>{item.answer}</p>}
           </li>
         ))}
       </ul>
